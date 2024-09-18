aa510d288b0ee80576e5d53103475bd1fea7933e
Juanerros <juancottier0@gmail.com>
2024-09-18 20:22:40 -0300

    first commit

---
+++ capa_datos/conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace capa_datos
+{
+    public class conexion : IDisposable
+    {
+        SqlConnection path = new SqlConnection("Data Source=CUANNET\\SQLEXPRESS;Initial Catalog=ferreteria2;Integrated Security=True;Encrypt=False");
+
+        public SqlConnection conect()
+        {
+            path.Open();
+            return path;
+        }
+
+        public void Dispose()
+        {
+            path.Close();
+        }
+    }
+}
